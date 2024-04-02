a2cf16c35510c4125be95843dbac48af45d62e96
m2612 <h-tsujigiwa@mtn.co.jp>
2024-04-02 18:05:08 +0900

    標準広角カメラをデフォルトに設定する

--- src/ios/CameraSessionManager.m
+++ src/ios/CameraSessionManager.m
@@ -716,12 +716,21 @@
 
 // Find a camera with the specified AVCaptureDevicePosition, returning nil if one is not found
 - (AVCaptureDevice *) cameraWithPosition:(AVCaptureDevicePosition) position {
-  NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-  for (AVCaptureDevice *device in devices){
-    if ([device position] == position)
-      return device;
-  }
-  return nil;
+  // NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+  // for (AVCaptureDevice *device in devices){
+  //   if ([device position] == position)
+  //     return device;
+  // }
+  // return nil;
+  // Use AVCaptureDeviceDiscoverySession to find the wide angle camera
+    AVCaptureDeviceDiscoverySession *discoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera] mediaType:AVMediaTypeVideo position:position];
+    NSArray<AVCaptureDevice *> *devices = discoverySession.devices;
+    for (AVCaptureDevice *device in devices) {
+        if (device.position == position) {
+            return device;
+        }
+    }
+    return nil;
 }
 
 @end
